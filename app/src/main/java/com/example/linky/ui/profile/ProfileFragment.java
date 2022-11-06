812d5f56dd168b8eeaf02217ad4c9f5ea2a3dfcc
Mircea Feder <federmircea@gmail.com>
2022-11-06 18:33:48 +0200

    created settings, account edit fragment and dialogs, finishing the UI

--- app/src/main/java/com/example/linky/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/linky/ui/profile/ProfileFragment.java
@@ -1,15 +1,20 @@
 package com.example.linky.ui.profile;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.linky.R;
 import com.example.linky.adapters.HomeAdapter;
 import com.example.linky.backend.models.EditableLink;
 import com.example.linky.databinding.FragmentProfileBinding;
@@ -19,11 +24,16 @@ import java.util.UUID;
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    private Dialog editLink;
+    private DisplayMetrics metrics = new DisplayMetrics();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         ProfileViewModel homeViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
         binding = FragmentProfileBinding.inflate(inflater, container, false);
+
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(metrics);
 
         EditableLink[] editableLinks = new EditableLink[]{
                 new EditableLink("facebook", "www.facebook.com/mircea-feder", UUID.randomUUID().toString()),
@@ -44,6 +54,12 @@ public class ProfileFragment extends Fragment {
         HomeAdapter adapter = new HomeAdapter(this.getContext(), editableLinks);
         binding.myLinksList.setAdapter(adapter);
         binding.myLinksList.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        editLink = new Dialog(getContext());
+        editLink.setContentView(R.layout.edit_link_dialog);
+        editLink.getWindow().setLayout((int) (metrics.widthPixels * 0.95), ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        binding.buttonShareMe.setOnClickListener(view -> editLink.show());
 
         return binding.getRoot();
     }
